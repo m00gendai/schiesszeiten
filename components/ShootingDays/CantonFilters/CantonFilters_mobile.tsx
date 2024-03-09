346edbd12c259c9a116b3d5ad10149e8baff6024
m00gendai <mrweber@gmx.ch>
2024-03-09 03:41:54 +0100

    Separate mobile canton filter

---
+++ components/ShootingDays/CantonFilters/CantonFilters_mobile.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import React, { useRef } from "react"
+import { swissCantons } from "../lib"
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime"
+import { ReadonlyURLSearchParams, usePathname, useRouter, useSearchParams } from "next/navigation"
+import s from "./CantonFilters.module.css"
+import Image from "next/image"
+import { setParams } from "../utils"
+import { useDisclosure } from '@mantine/hooks';
+import { Drawer, Button } from '@mantine/core';
+
+function setCantons(router:AppRouterInstance, path:string, params:ReadonlyURLSearchParams, checkboxRef:React.RefObject<(HTMLInputElement[] | null)>, index:number, canton:string){
+    const cantonParams = params.get("cantons")
+    const currentCantons = cantonParams?.length === 0 ? [] : cantonParams?.split(",") || []
+    const newParams = new URLSearchParams(params) 
+    newParams.delete("page")
+    if(!currentCantons.includes(canton)){
+        const newCantons = [...currentCantons, canton]
+        newParams.delete("cantons")
+        newParams.append("cantons", newCantons.join(","))
+        setParams(router,path,newParams)
+    } else {
+        const newCantons = currentCantons.filter(element => element !== canton)
+        if(newCantons.length === 0){
+            newParams.delete("cantons")
+            setParams(router,path,newParams)
+        } else {
+            newParams.delete("cantons")
+            newParams.append("cantons", newCantons.join(","))
+            setParams(router,path,newParams)
+        }
+    }
+    
+    const box = checkboxRef.current![index]
+    box.checked = !box.checked
+}
+
+
+
+export default function CantonFilters_mobile(){
+
+    const router:AppRouterInstance = useRouter()
+    const path:string = usePathname()
+    const params:ReadonlyURLSearchParams = useSearchParams()
+    const checkboxRef = useRef<(HTMLInputElement[] | null)>([])
+    const [opened, { open, close }] = useDisclosure(false);
+
+
+    const cantonParams = params.get("cantons")
+    const currentCantons = cantonParams?.split(",") || []
+    
+    return(
+        <>
+        <Drawer opened={opened} onClose={close} title="Kantone" className={s.mobile} size={"100%"} lockScroll={false}>
+            <div className={s.container}>
+                {swissCantons.map((canton, index)=>{
+                    return(
+                        <div className={s.box} key={`checkbox_canton_${canton}`} onClick={()=>setCantons(router, path, params, checkboxRef, index, canton)}>
+                            <Image
+                                src={`/${canton}.png`}
+                                fill={true}
+                                style={{objectFit: "contain"}}
+                                alt={`${canton}`}
+                            />
+                            <label className={s.label} htmlFor={`checkbox_canton_${canton}`}>{canton}</label>
+                            {/*@ts-expect-error */}
+                            <input ref={(element)=>checkboxRef.current[index] = element} className={s.input} type="checkbox" id={`checkbox_canton_${canton}`} onChange={()=>setCantons(router, path, params, checkboxRef, index, canton)} defaultChecked={currentCantons.includes(canton) ? true : false} ></input>
+                        </div>
+                    )
+                })}
+            </div>
+        </Drawer>
+        <Button className={s.mobile} onClick={open}>Kantone</Button>
+        </>
+    )
+}
