920988b4bea6c17a44ff8db16528d3b33f8f040d
m00gendai <mrweber@gmx.ch>
2024-03-12 19:44:24 +0100

    Removed comma

--- components/ShootingDays/MapView/Interface_MapView.ts
+++ components/ShootingDays/MapView/Interface_MapView.ts
@@ -1,6 +1,6 @@
 export interface MarkerData{
     coordinates: { easting: string, northing: string }
-    place: string,
+    place: string
     club: string
     event: string
     dateStart: string
